38f1e54269f5633a5d02d2e4bcd1b08835973517
Maryll Castelino <maryllcastelino@gmail.com>
2023-01-08 19:28:08 +0530

    dependency injection

--- src/main/java/com/example/springbootdemo/student/StudentController.java
+++ src/main/java/com/example/springbootdemo/student/StudentController.java
@@ -3,6 +3,7 @@ package com.example.springbootdemo.student;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
--- src/main/java/com/example/springbootdemo/student/StudentService.java
+++ src/main/java/com/example/springbootdemo/student/StudentService.java
@@ -3,6 +3,10 @@ package com.example.springbootdemo.student;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
     public List<Student> getStudents() {
 		return List.of(
